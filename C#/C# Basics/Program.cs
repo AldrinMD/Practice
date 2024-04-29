ffa97b3d4f220a142d437348a9bf0afc0c6f8767
AldrinMD <72550659+AldrinMD@users.noreply.github.com>
2024-04-29 15:45:20 +0100

    Update: C# Practice

---
+++ C#/C# Basics/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace C__Basics 
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("What Would you like to do? ");
+            string action = Console.ReadLine().ToLower();
+
+            Test test = new Test();
+
+            if (action == "add") 
+            {
+                Console.WriteLine($"Answer: {test.add()}");
+            }
+
+        }
+    }
+
+    public class Test 
+    {
+        public int add() 
+        {
+            int answer = 0;
+            bool endIf = false;
+
+            while (endIf != true) 
+            {
+                Console.WriteLine("Type number: (Enter = to calculate)");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "=")
+                {
+                    endIf = true;
+                }
+                else if (Int32.TryParse(userInput, out var convertedInput)) {
+                    answer += Convert.ToInt32(convertedInput);
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong please type a number to add or = to calculate.");                  
+                }
+            }
+
+            return answer; 
+        }
+
+    }
+
+
+
+
+
+
+}
